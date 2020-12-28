e90813a3068127d257ba7e9138fe3bb996cfe2e3
andreabefuen <andreabenitofuentes@gmail.com>
2020-12-28 18:41:39 +0100

    Gripping animations working, fixed models for Oculus Quest Contollers

--- Assets/TFM/Scripts/HandPresence.cs
+++ Assets/TFM/Scripts/HandPresence.cs
@@ -5,21 +5,111 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    public bool showContoller = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+   
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
+        TryInitialize();
+
+    }
+
+    void TryInitialize()
+    {
         List<InputDevice> devices = new List<InputDevice>();
-        InputDevices.GetDevices(devices);
+        InputDeviceCharacteristics rightControllerCharacter = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        //InputDevices.GetDevices(devices);
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacter, devices);
 
         foreach (var item in devices)
         {
-            Debug.Log(item.name + item.characteristics);
+            Debug.LogError(item.name + item.characteristics);
+        }
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+                spawnedController = Instantiate(prefab, transform);
+            else
+            {
+                Debug.LogError("Did not find corresponding controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+        }
+        spawnedHandModel = Instantiate(handModelPrefab, transform);
+        handAnimator = spawnedHandModel.GetComponent<Animator>();
+    }
+
+    void UpdateAnimation()
+    {
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+        else
+        {
+            //TestInputsController();
+
+            if (showContoller)
+            {
+                spawnedHandModel.SetActive(false);
+                spawnedController.SetActive(true);
+            }
+            else
+            {
+                spawnedHandModel.SetActive(true);
+                spawnedController.SetActive(false);
+                UpdateAnimation();
+            }
+        }
+
+    }
+
+    void TestInputsController()
+    {
+        //For Primary button
         
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+            Debug.Log("Pressing primary button");
+
+        
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+            Debug.Log("Trigger pressed  " + triggerValue);
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DValue) && primary2DValue != Vector2.zero)
+            Debug.Log("Primary touchpad " + primary2DValue);
+
     }
 }
