2097016daf52a619ba17419d0197c1bf6481e703
andreabefuen <andreabenitofuentes@gmail.com>
2021-02-17 17:38:20 +0100

    Cleaning the project

---
+++ Assets/Scripts/Components/Timers/TestTimer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestTimer : MonoBehaviour
+{
+    public Text showingText;
+
+    public void UpdateTimeText(float duration)
+    {
+        showingText.text = "Duration: " + duration;
+    }
+}
