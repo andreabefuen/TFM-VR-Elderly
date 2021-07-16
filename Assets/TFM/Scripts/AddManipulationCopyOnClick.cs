524d0c1ed84212f40fa370c192e92fa3223566a2
andreabefuen <andreabenitofuentes@gmail.com>
2021-07-16 18:24:23 +0200

    Package MRTK and MRTK_Scene for modify room

---
+++ Assets/TFM/Scripts/AddManipulationCopyOnClick.cs
@@ -0,0 +1,37 @@
+using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddManipulationCopyOnClick : MonoBehaviour
+{
+    [SerializeField]
+    private Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Interactable interactableComponent = this.GetComponent<Interactable>();
+        interactableComponent.OnClick.AddListener(Copy);
+
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
+    }
+    public void Copy()
+    {
+        GameObject copy = Instantiate(this.gameObject);
+        copy.transform.position = spawnPoint.position;
+        copy.transform.rotation = spawnPoint.rotation;
+        copy.transform.up = Vector3.up;
+        copy.transform.localScale = spawnPoint.localScale;
+        
+        copy.AddComponent<ObjectManipulator>();
+        BoundingBox boundingBox = copy.AddComponent<BoundingBox>();
+        boundingBox.BoundingBoxActivation = BoundingBox.BoundingBoxActivationType.ActivateByProximityAndPointer;
+
+        RotationAxisConstraint rotationAxisConstraint = copy.AddComponent<RotationAxisConstraint>();
+        rotationAxisConstraint.ConstraintOnRotation = Microsoft.MixedReality.Toolkit.Utilities.AxisFlags.XAxis | Microsoft.MixedReality.Toolkit.Utilities.AxisFlags.ZAxis;
+
+        Destroy(copy.GetComponent<Interactable>());
+        Destroy(copy.GetComponent<AddManipulationCopyOnClick>());
+    }
+}
--- Assets/TFM/Scripts/LocomotionController.cs
+++ Assets/TFM/Scripts/LocomotionController.cs
@@ -3,45 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class LocomotionController : MonoBehaviour
+
+namespace TFM.VRScripts
 {
-    public XRController leftTeleportRay;
-    public XRController rightTeleportRay;
-
-    public InputHelpers.Button teleportActivationButton;
-    public float activationThreshold = 0.1f;
-
-    public XRRayInteractor leftInteractorRay;
-    public XRRayInteractor rightInteractorRay;
-
-    public bool EnableLeftTeleport { get; set; } = true;
-    public bool EnableRightTeleport { get; set; } = true;
-
-
-    // Update is called once per frame
-    void Update()
+    public class LocomotionController : MonoBehaviour
     {
-        Vector3 pos = new Vector3();
-        Vector3 norm = new Vector3();
-        int index = 0;
-        bool validTarget = false;
+        public XRController leftTeleportRay;
+        public XRController rightTeleportRay;
 
-        if (leftInteractorRay)
+        public InputHelpers.Button teleportActivationButton;
+        public float activationThreshold = 0.1f;
+
+        public XRRayInteractor leftInteractorRay;
+        public XRRayInteractor rightInteractorRay;
+
+        public bool EnableLeftTeleport { get; set; } = true;
+        public bool EnableRightTeleport { get; set; } = true;
+
+
+        // Update is called once per frame
+        void Update()
         {
-            bool isLeftInteractorRayHovering = leftInteractorRay.TryGetHitInfo(out pos, out norm, out index, out validTarget);
-            leftTeleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftTeleportRay) && !isLeftInteractorRayHovering);
-        }
-        if (rightInteractorRay)
-        {
-            bool isRightInteractorRayHovering = rightInteractorRay.TryGetHitInfo(out pos, out norm, out index, out validTarget);
-            rightTeleportRay.gameObject.SetActive(EnableRightTeleport && CheckIfActivated(rightTeleportRay) && !isRightInteractorRayHovering);
+            Vector3 pos = new Vector3();
+            Vector3 norm = new Vector3();
+            int index = 0;
+            bool validTarget = false;
+
+            if (leftInteractorRay)
+            {
+                bool isLeftInteractorRayHovering = leftInteractorRay.TryGetHitInfo(out pos, out norm, out index, out validTarget);
+                leftTeleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftTeleportRay) && !isLeftInteractorRayHovering);
+            }
+            if (rightInteractorRay)
+            {
+                bool isRightInteractorRayHovering = rightInteractorRay.TryGetHitInfo(out pos, out norm, out index, out validTarget);
+                rightTeleportRay.gameObject.SetActive(EnableRightTeleport && CheckIfActivated(rightTeleportRay) && !isRightInteractorRayHovering);
+            }
+
         }
 
+        public bool CheckIfActivated(XRController controller)
+        {
+            InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+            return isActivated;
+        }
     }
 
-    public bool CheckIfActivated(XRController controller)
-    {
-        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
-        return isActivated;
-    }
 }
